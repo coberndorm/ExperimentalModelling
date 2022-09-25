f9cf9b0b212988885ea87b0154c49be68acd70e8
coberndorm <coberndorm@eafit.edu.co>
2022-09-25 17:49:13 -0500

    Arreglado algunas cosas y hecho unos parciales

--- Funciones/KalmanExtended.m
+++ Funciones/KalmanExtended.m
@@ -1,10 +1,13 @@
-function x_est = KalmanExtended(A,C,odes,t,x,u,y,x0,M,R,P0,Ts)
+function [P_trace, K_norm, x_est] = KalmanExtended(A,C,odes,t,x,u,y,x0,M,R,P0,Ts,V)
 %KALMAN Summary of this function goes here
 %   Detailed explanation goes here
 
 %Inicializando variables importantes
 n = length(y); x_est = zeros(n,2); P_trace = zeros(n,1); K_norm = zeros(n,1);
 
+if nargin < 13
+    V = ones(n,2);
+end
 %Definiendo A como una funcion para poder evaluar facilmente en ella
 syms T
 A = eye(2) + A*T; A = matlabFunction(A);
@@ -33,22 +36,18 @@ for k=1:n
     catch
         x_pred = x_act + Ts*odes(x_act(1),x_act(2));
     end
-    P_pred = A_i*P_act*A_i' + M;
+
+    P_pred = A_i*P_act*A_i' + V(k,:)*M*V(k,:)';
     % Corrección:
     K = (P_pred*C_i')*inv(C_i*P_pred*C_i' + R);
     x_act = x_pred + K*(y(k) - C_i*x_pred);
     P_act = (eye(2) - K*C_i)*P_pred;
     % Estimación:
     x_est(k,:) = x_act';
-    % Medidas:
-    P_trace(k) = trace(P_act);
+    % Medidas:p
+    P_trace(k) = trace(P_pred);
     K_norm(k) = norm(K);
 end
-plot(t, x(:,2), t, x_est(:,2))
-legend({'Modelo original', 'Estimación Kalman Extendido'}, 'Location', 'best');
-xlabel('$k$', 'Interpreter', 'Latex', 'FontSize', 15);
-ylabel('$x_{2}(k)$', 'Interpreter', 'Latex', 'FontSize', 15);
-title('Comparación modelo original con estimación kalman extendido', 'FontSize', 12);
-grid on
+
 end
 
